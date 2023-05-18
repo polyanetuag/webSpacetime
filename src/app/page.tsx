6b1599ef062b15c00902341ad5de38cbf949e2f0
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-18 19:47:41 -0300

    feat: left side Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,49 @@
+import { User } from 'lucide-react'
+import nlwLogo from '../assets/logo-spacetime.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* LEFT */}
-      <div className=" relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      <div className=" border-white/10 bg-cover relative flex flex-col items-start justify-between overflow-hidden border-r bg-[url(../assets/bg-stars.svg)] px-28 py-16">
         <div className="absolute right-0 top-1/2  h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className=" absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* SIGN IN */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="text-sm max-w-[140px] leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias
+          </p>
+        </a>
+
+        {/* HERO */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a className="inline-block" href="">
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
       </div>
 
       {/* RIGHT */}
-      <div className="flex flex-col p-16">
+      <div className="bg-cover flex flex-col bg-[url(../assets/bg-stars.svg)] p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
