b1ca5823a10ceebb0f51adc52e5aa644889cef02
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-19 13:59:10 -0300

    feat: added button and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,17 +27,34 @@ export default function Home() {
         {/* HERO */}
         <div className="space-y-5">
           <Image src={nlwLogo} alt="NLW Spacetime" />
+
           <div className="max-w-[420px] space-y-1">
-            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
               Sua cápsula do tempo
             </h1>
-            <p>
+            <p className="text-lg leading-relaxed">
               Colecione momentos marcantes da sua jornada e compartilhe (se
               quiser) com o mundo!
             </p>
           </div>
-          <a className="inline-block" href="">
+          <a
+            className="text-sm inline-block rounded-full bg-green-500 p-3 px-5 font-alt uppercase leading-none text-gray-900 hover:bg-green-600"
+            href=""
+          >
             CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* FOOTER/COPYRIGHT */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
           </a>
         </div>
       </div>
